b88599f146c681f9658f7d3f958d25a4f576a4f2
Villode <vip520moyan@gmail.com>
2023-04-01 14:39:21 +0000

    Update pages.yml

--- js/nav.js
+++
@@ -1,7 +0,0 @@
-function navTitle() {
-    let e = document.title;
-    document.getElementById("page-name-text").innerHTML = e.replace(" | JayHrn", "")
-}
-
-navTitle() // 打开网站先执行一次
-document.addEventListener("pjax:complete", navTitle) // pjax加载完成（切换页面）后再执行一次
