41796fe287cc44f77f377a071447fb725fd59bd3
Villode <vip520moyan@gmail.com>
2023-09-20 09:27:54 +0000

    Update _config.yml

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2014/06/01/5g/","2022/03/25/IDEA使用JSP用户登录案例/","2023/08/03/cloudflare_1.1.1.1/","2022/03/25/data_bl/","2014/06/01/yun/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2014/05/31/5g/","2022/03/24/IDEA使用JSP用户登录案例/","2023/08/03/cloudflare_1.1.1.1/","2022/03/24/data_bl/","2014/05/31/yun/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
