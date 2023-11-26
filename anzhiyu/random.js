d0a36cd70f99475fd2657ade091fb9f56c55fdf5
Villode <vip520moyan@gmail.com>
2023-11-26 16:31:52 +0000

    Update pages.yml

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2014/05/31/5g/","2023/08/03/cloudflare_1.1.1.1/","2022/03/24/IDEA使用JSP用户登录案例/","2022/03/24/data_bl/","2014/05/31/yun/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
