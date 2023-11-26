64c37261696e9e0a76a20783f61a0b2fda78c9c8
Villode <vip520moyan@gmail.com>
2023-11-26 16:04:40 +0000

    Update _config.Acrylic.yml

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2014/05/31/5g/","2022/03/24/IDEA使用JSP用户登录案例/","2023/08/03/cloudflare_1.1.1.1/","2022/03/24/data_bl/","2014/05/31/yun/"];function toRandomPost(){
+var posts=["2014/05/31/5g/","2023/08/03/cloudflare_1.1.1.1/","2022/03/24/IDEA使用JSP用户登录案例/","2022/03/24/data_bl/","2014/05/31/yun/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
